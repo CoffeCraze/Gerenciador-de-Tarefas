c62aee26e3000652dec9cf6719a7e2b9db048a11
Arthur Ferreira Borges <athurborges.ferreiragithub@gmail.com>
2025-07-08 22:31:09 -0300

    Update App.jsx
    Arrumando o fundo para que todos os dispositivos fique alinhado

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function App() {
   return (
     console.log("App component loaded"),
     (
-      <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-full h-full bg-slate-500 flex justify-center p-6">
         <div className="w-[500px] space-y-4">
           <Title>Gerenciador de Tarefas</Title>
           <AddTasks onTasksAddSubmit={onTasksAddSubmit} />
